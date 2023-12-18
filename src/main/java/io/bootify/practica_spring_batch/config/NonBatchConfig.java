6b7bf7f97caab462ea78705392f2799bd6de0bba
Silvia Lorero Salor <1004291182alumnos.uc3m.es>
2023-12-18 19:25:03 +0100

    cambios

---
+++ src/main/java/io/bootify/practica_spring_batch/config/NonBatchConfig.java
@@ -0,0 +1,64 @@
+package io.bootify.practica_spring_batch.config;
+
+import io.bootify.practica_spring_batch.domain.Transacciones;
+import io.bootify.practica_spring_batch.repos.TransaccionesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+/*import org.springframework.context.annotation.Configuration;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+public class NonBatchConfig {
+
+    @Autowired
+    private TransaccionesRepository transaccionesRepository;
+
+    @Bean
+    public void importCsvToDBJob() {
+        try {
+            List<Transacciones> transaccionesList = readCsvFile("src/main/resources/transacciones.csv");
+
+            for (Transacciones transaccion : transaccionesList) {
+                transaccionesRepository.save(transaccion);
+            }
+
+            System.out.println("Import completed successfully.");
+        } catch (IOException | DateTimeParseException | NumberFormatException e) {
+            e.printStackTrace();
+            System.err.println("Error during CSV import: " + e.getMessage());
+        }
+    }
+
+    private List<Transacciones> readCsvFile(String filePath) throws IOException {
+        List<Transacciones> transaccionesList = new ArrayList<>();
+        try (BufferedReader reader = Files.newBufferedReader(Paths.get(filePath))) {
+            // Omitir la primera línea que contiene los encabezados
+            String headerLine = reader.readLine();
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] values = line.split(",");
+                Transacciones transaccion = new Transacciones();
+                transaccion.setId(Long.parseLong(values[0]));
+                transaccion.setFecha(LocalDate.parse(values[1])); // Assuming date is in ISO format (yyyy-MM-dd)
+                transaccion.setCantidad(Double.parseDouble(values[2]));
+                transaccion.setTipoTrans(values[3]);
+                transaccion.setCuentaOrigen(values[4]);
+                transaccion.setCuentaDestino(values[5]);
+                transaccionesList.add(transaccion);
+            }
+        }
+        return transaccionesList;
+    }
+
+}
+
+*/
--- src/main/java/io/bootify/practica_spring_batch/controller/JobController.java
+++ src/main/java/io/bootify/practica_spring_batch/controller/JobController.java
@@ -1,38 +1,85 @@
 package io.bootify.practica_spring_batch.controller;
+/*
 
-
+import io.bootify.practica_spring_batch.domain.Transacciones;
+import io.bootify.practica_spring_batch.repos.TransaccionesRepository;
 import org.springframework.batch.core.*;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
 import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
 import org.springframework.batch.core.repository.JobRestartException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/jobs")
 public class JobController {
 
     @Autowired
-    private JobLauncher jobLauncher;
-    @Autowired
-    private Job job;
+    private TransaccionesRepository transaccionesRepository;
 
     @PostMapping("/importTransacciones")
-    public void importCsvToDBJob() {
-        JobParameters jobParameters = new JobParametersBuilder()
-                .addLong("startAt", System.currentTimeMillis()).toJobParameters();
+    public ResponseEntity<String> importCsvToDBJob() {
         try {
-            jobLauncher.run(job, jobParameters);
-        } catch (JobExecutionAlreadyRunningException | JobRestartException | JobInstanceAlreadyCompleteException | JobParametersInvalidException e) {
+            List<Transacciones> transaccionesList = readCsvFile("src/main/resources/transacciones.csv");
+
+            for (Transacciones transaccion : transaccionesList) {
+                transaccionesRepository.save(transaccion);
+            }
+
+            return ResponseEntity.ok("Import completed successfully.");
+        } catch (IOException e) {
             e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error during CSV import: " + e.getMessage());
         }
     }
 
+    private List<Transacciones> readCsvFile(String filename) throws IOException {
+        List<Transacciones> transaccionesList = new ArrayList<>();
+        try (InputStream inputStream = getClass().getClassLoader().getResourceAsStream(filename)) {
+            if (inputStream != null) {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    String[] values = line.split(",");
+                    Transacciones transaccion = new Transacciones();
+                    transaccion.setId(Long.parseLong(values[0]));
+                    transaccion.setFecha(LocalDate.parse(values[1]));
+                    transaccion.setCantidad(Double.parseDouble(values[2]));
+                    transaccion.setTipoTrans(values[3]);
+                    transaccion.setCuentaOrigen(values[4]);
+                    transaccion.setCuentaDestino(values[5]);
+                    transaccionesList.add(transaccion);
+                }
+            } else {
+                throw new IOException("CSV file not found: " + filename);
+            }
+        }
+        return transaccionesList;
+    }
 }
 
 
 
+
+*/
